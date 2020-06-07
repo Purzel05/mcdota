f56cd5f279d625d318907ef4d21fbc6c89087f83
Leonard Prentl <leo.prentl@gmail.com>
2020-06-07 18:50:37 +0200

    g

--- src/main/java/de/prentl/firsttestproject/commands/WorldCommandExecutor.java
+++ src/main/java/de/prentl/firsttestproject/commands/WorldCommandExecutor.java
@@ -2,6 +2,7 @@ package de.prentl.firsttestproject.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,21 +18,30 @@ public class WorldCommandExecutor implements CommandExecutor {
                 player.sendMessage("§cBitte benutze /worlds help");
             } else {
                 if (args[0].equalsIgnoreCase("help")) {
-                    player.sendMessage("§e/world create <Name> [Seed] §7- §eErstellt eine Welt");
+                    player.sendMessage("§e/world create <Name> [Type] [Seed] §7- §eErstellt eine Welt");
                     player.sendMessage("§e/world tp <Name> §7- §eTeleportiert dich zu genannter Welt");
                 } else if (args[0].equalsIgnoreCase("tp")){
                     if(args.length == 2){
                         if(Bukkit.getWorld(args[1]) != null){
                             player.teleport(Bukkit.getWorld(args[1]).getSpawnLocation());
+                        }else{
+                            System.out.println("World ist null");
                         }
                     }
 
-                }else if(args[0].equalsIgnoreCase("create")){
+                } else if(args[0].equalsIgnoreCase("create")){
                     if(args.length >= 2){
                         WorldCreator creator = new WorldCreator(args[1]);
 
-                        if(args.length >= 3) {
-                            creator.seed(Long.parseLong(args[2]));
+                        if (args.length >= 3 && args[2].equalsIgnoreCase("flat")){
+                            creator.type(WorldType.FLAT);
+                            creator.generateStructures(false);
+                        } else {
+                            creator.type(WorldType.NORMAL);
+                        }
+
+                        if(args.length >= 4) {
+                            creator.seed(Long.parseLong(args[3]));
                         }
                         player.sendMessage("§7Welt wird erstellt...");
                         Bukkit.createWorld(creator);
